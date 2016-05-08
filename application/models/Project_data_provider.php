81d6c037bbda3c333d0137e43add8f60d7154d92
zhourunlai <u201316277@hust.edu.cn>
2016-05-08 22:47:15 +0800

    test

--- application/models/Project_data_provider.php
+++ application/models/Project_data_provider.php
@@ -31,9 +31,9 @@ class Project_data_provider extends CI_Model
     {
         $sql = "SELECT * FROM $this->table WHERE id=?";
         $project_info_list = $this->db->query($sql, $id)->result_array();
-        if (count($project_info_list <= 0)) {
-            return false;
-        }
+//        if (count($project_info_list <= 0)) {
+//            return false;
+//        }
         return $project_info_list[0];
     }
 
