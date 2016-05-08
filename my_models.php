8e8ba972b9e46375dc646bf5d9625ca98d1f0792
zhourunlai <u201316277@hust.edu.cn>
2016-05-08 22:20:07 +0800

    test

---
+++ my_models.php
@@ -0,0 +1,24 @@
+<?php die();
+
+/**
+ * Add you custom models here that you are loading in your controllers
+ *
+ * <code>
+ * $this->site_model->get_records()
+ * </code>
+ * Where site_model is the model Class
+ *
+ * ---------------------- Models to Load ----------------------
+ * <examples>
+ *
+ * @property member_data_provider $member_data_provider
+ * @property project_data_provider $project_data_provider
+ * @property team_data_provider $team_data_provider
+ * @property user_data_provider $user_data_provider
+ */
+
+class my_models
+{
+}
+
+// End my_models.php
